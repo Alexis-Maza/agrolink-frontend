8bf78db1403c78391156ebe0dca4a94fe14a851c
Alexis15Maza <u23233005@utp.edu.pe>
2026-05-18 16:37:57 -0500

    Nuevos apartados de la arquitectura

--- agrolink-frontend/src/index.js
+++ agrolink-frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+/* Importación del diseño base (Afecta a todo el sitio y a Public) */
+import './styles/theme.css';
+
+/* Importación de las variaciones de estilo para cada rol */
+import './styles/public.css';
+import './styles/admin.css';
+import './styles/farmer.css';
+import './styles/buyer.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +18,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
